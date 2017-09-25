1b62aab5983c52200edb56db6be5ef9db921e509
K <kiyunkim@users.noreply.github.com>
2017-09-25 11:06:39 -0400

    add initial code

---
+++ script.js
@@ -0,0 +1,20 @@
+var expNano = (function(option) {
+  var self = this,
+      proto = expNano.prototype,
+
+      container = option.container;
+  
+  proto.setup = function() {
+    $(container).show();
+  };
+  return {
+    setup: self.setup
+  };
+});
+
+$(document).ready(function() {
+  var expNanoBar = new expNano({
+    container: '.expandNanoContainer'
+  });
+  expNanoBar.setup();
+});
