9adab817fa94ea7f1d61010fae74a77b64c90413
K <kiyunkim@users.noreply.github.com>
2017-12-14 14:54:35 -0500

    update html scss js to fit slick

--- script.js
+++ script.js
@@ -1,59 +1,36 @@
-var expNano = (function(option) {
-  var self = this,
-      proto = expNano.prototype,
-
-      container = option.container,
-      $containerClone,
-      showingClass = option.showingClass,
-      bottomContainerClass = option.bottomContainerClass,
-      toggleTab = option.toggleTab,
-      expandArrow = option.expandArrow,
-      sliderAnimate = option.sliderAnimate,
-      sliderWidth,
-      toggleContainer = option.toggleContainer;
-  
-  function toggleFunc() {
-    $(toggleTab).click(function() {
-      $(this).find(expandArrow).toggleClass(showingClass);
-      $(this).next(toggleContainer).slideToggle(300);
-    });
-  }
-  function init() {
-    $containerClone = $(container).clone().addClass(bottomContainerClass);
-
-    $containerClone.appendTo('body');
-    $(container).not(bottomContainerClass).show();
-  }
-
-  function minifyOnScroll() {
-    $(window).scroll(function(){
-      /* change 0(px) to toggle it further down
-      if (($(this).scrollTop() > 0) && ($(input).is(':checked'))) {
-        toggleBar();
-      } 
-      */
-    });
-  }
-  
-  proto.setup = function() {
-    init();
-    toggleFunc();
-    minifyOnScroll();
-  };
-  return {
-    setup: self.setup
-  };
-});
 
 $(document).ready(function() {
-  var expNanoBar = new expNano({
-    container: '.expandNanoContainer',
-    bottomContainerClass: 'bottom-fixed',
-    showingClass: 'active',
-    toggleTab: '.toggleTab',
-    expandArrow: '.expandNanoButton',
-    sliderAnimate: '.bigNanoAnimate',
-    toggleContainer: '.bigNanoContainer'
+  $('.bigNanoSlider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
-  expNanoBar.setup();
 });
